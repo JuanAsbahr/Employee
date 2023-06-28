8bf01cf27620b58cae1ba6032b97f86a81603921
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-28 16:39:19 -0300

    Employee Class

---
+++ Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Employees.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+    }
+}
